62a8ff83c1a87eb6f6f4beccb0a0aac795038f2c
Igor Oliinyk <igor.oleynikdp@gmail.com>
2019-02-17 16:47:34 +0200

    Реализция остальных обработчиков

--- app.js
+++ app.js
@@ -15,6 +15,36 @@ function validateName() {
     name.classList.remove("is-invalid");
   }
 }
-function validateZip() {}
-function validateEmail() {}
-function validatePhone() {}
+function validateZip() {
+  const zip = document.getElementById("zip");
+
+  const re = /^[0-9]{5}(-[0-9]{4})?$/;
+
+  if (!re.test(zip.value)) {
+    zip.classList.add("is-invalid");
+  } else {
+    zip.classList.remove("is-invalid");
+  }
+}
+function validateEmail() {
+  const email = document.getElementById("email");
+
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,6})$/;
+
+  if (!re.test(email.value)) {
+    email.classList.add("is-invalid");
+  } else {
+    email.classList.remove("is-invalid");
+  }
+}
+function validatePhone() {
+  const phone = document.getElementById("phone");
+
+  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+  if (!re.test(phone.value)) {
+    phone.classList.add("is-invalid");
+  } else {
+    phone.classList.remove("is-invalid");
+  }
+}
