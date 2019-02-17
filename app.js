7c0e7406ddf8264b87f31295078208081b998df5
Igor Oliinyk <igor.oleynikdp@gmail.com>
2019-02-17 16:28:48 +0200

    Добавлены слушатели событий Обработчики И реализация name

---
+++ app.js
@@ -0,0 +1,20 @@
+// Form blur event listeners
+document.getElementById("name").addEventListener("blur", validateName);
+document.getElementById("zip").addEventListener("blur", validateZip);
+document.getElementById("email").addEventListener("blur", validateEmail);
+document.getElementById("phone").addEventListener("blur", validatePhone);
+
+function validateName() {
+  const name = document.getElementById("name");
+
+  const re = /^[a-zA-Z]{2,10}$/;
+
+  if (!re.test(name.value)) {
+    name.classList.add("is-invalid");
+  } else {
+    name.classList.remove("is-invalid");
+  }
+}
+function validateZip() {}
+function validateEmail() {}
+function validatePhone() {}
